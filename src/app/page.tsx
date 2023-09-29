7d79bdb71e53ad8d8ed01b0db8231e85ea02ee23
Emily Lubkert <elubkert@questarai.com>
2023-09-29 14:27:48 -0600

    added S3, create-chat route, updated fileupload and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import { Button } from "@/components/ui/button";
 import { UserButton, auth } from "@clerk/nextjs/app-beta";
 import Link from "next/link";
-import {LogIn} from 'lucide-react'
+import { LogIn } from 'lucide-react'
 import FileUpload from "@/components/FileUpload";
-
+//stop at minute 55
 
 export default async function Home() {
   const { userId } = await auth();
@@ -14,22 +14,22 @@ export default async function Home() {
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
-            <h1 className="mp-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <h1 className="mr-4 text-5xl font-semibold">Chat with any PDF</h1>
             <UserButton afterSignOutUrl="/" />
           </div>
-          <div className="flex mt-2">
+          <div className="flex mt-2 ">
             {isAuth && <Button>Go to Chats</Button>}
           </div>
 
           <p className="max-w-xl mt-1 text-lg text-slate-600">
-            Join millions of students, researchers, and professionals to instantly answer questions and understand research with AI.
+            Join students, researchers, and professionals to instantly answer questions and understand research with AI.
           </p>
           <div className="w-full mt-4">
             {isAuth ? (<FileUpload />) : (
               <Link href='/sign-in'>
-              <Button>Login to get started
-                <LogIn className="w-4 h-4 ml-2" />
-              </Button>
+                <Button>Login to get started
+                  <LogIn className="w-4 h-4 ml-2" />
+                </Button>
               </Link>
             )}
           </div>
