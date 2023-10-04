2fbfbf64074685c5467ca27b7c01d9da598259f6
Emily Lubkert <elubkert@questarai.com>
2023-10-03 20:56:05 -0600

    added method for PDFs to download, split, vectorize, embed, and upload to pinecone

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -5,7 +5,7 @@ export const userSystemEnum = pgEnum('user_system_enum', ['system', 'user'])
 export const chats = pgTable('chats', {
     id: serial('id').primaryKey(),
     pdfName: text('pdf_name').notNull(),
-    prdUrl: text('pdf_url').notNull(),
+    pdfUrl: text('pdf_url').notNull(),
     createdAt: timestamp('created_at').notNull().defaultNow(),
     userId: varchar('user_id', { length: 256 }).notNull(),
     fileKey: text('file_key').notNull(),
